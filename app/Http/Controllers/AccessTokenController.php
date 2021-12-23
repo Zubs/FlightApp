314d107088f6f4fab6e0192afb5f4b4ae685dbf8
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2021-12-23 23:14:28 +0100

    Generate access token

---
+++ app/Http/Controllers/AccessTokenController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Session;
+
+class AccessTokenController extends Controller
+{
+    public function __invoke(Client $client)
+    {
+        $url = 'https://test.api.amadeus.com/v1/security/oauth2/token';
+
+        try {
+            $response = $client->post($url, [
+                'headers' => [
+                    'Accept' => 'application/json'
+                ],
+                'form_params' => [
+                    'grant_type' => 'client_credentials',
+                    'client_id' => env('AMADEUS_API_KEY'),
+                    'client_secret' => env('AMADEUS_API_SECRET')
+                ]
+            ]);
+
+            $response = $response->getBody();
+            $access_token = json_decode($response)->access_token;
+
+            return $response;
+        } catch (GuzzleException $exception) {
+            dd($exception);
+        }
+    }
+}
