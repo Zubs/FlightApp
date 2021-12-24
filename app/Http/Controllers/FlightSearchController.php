e28de2d918fd8631fa21f1f9602772e9b09eb395
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2021-12-24 01:32:24 +0100

    Search Flights POST

--- app/Http/Controllers/FlightSearchController.php
+++ app/Http/Controllers/FlightSearchController.php
@@ -8,35 +8,89 @@ use Illuminate\Http\Request;
 
 class FlightSearchController extends Controller
 {
+    /**
+     * Using the GET endpoint
+     * @param Request $request
+     * @param Client $client
+     * @return \Exception|GuzzleException|\Psr\Http\Message\StreamInterface
+     */
+//    public function __invoke (Request $request, Client $client)
+//    {
+//        $url = 'https://test.api.amadeus.com/v2/shopping/flight-offers';
+//        $access_token = '8OGlswlpw0H6jmzvNk3p2V9wlBzW';
+//
+//        $data = [
+//            'originLocationCode'     => 'BOS',
+//            'destinationLocationCode' => 'PAR',
+//            'departureDate'           => '2021-12-27',
+//            'adults'                  => 1
+//        ];
+//
+//        // To covert key value pairs into query parameters
+//        $data = http_build_query($data);
+//
+//        // Append the query parameters to the URL
+//        $url .= '?' . $data;
+//
+//        try {
+//            $response = $client->get($url, [
+//                'headers' => [
+//                    'Accept' => 'application/json',
+//                    'Authorization' => 'Bearer ' . $access_token
+//                ],
+//            ]);
+//
+//            return $response->getBody();
+//        } catch (GuzzleException $exception) {
+//            dd($exception);
+//        }
+//    }
+
+    /**
+     * Using the POST endpoint
+     * @param Request $request
+     * @param Client $client
+     * @return \Exception|GuzzleException|\Psr\Http\Message\StreamInterface
+     */
     public function __invoke (Request $request, Client $client)
     {
         $url = 'https://test.api.amadeus.com/v2/shopping/flight-offers';
-        $access_token = '8OGlswlpw0H6jmzvNk3p2V9wlBzW';
+        $access_token = 'v5ZXlshtteBTGn2bcmhWbxcPDHGa';
 
         $data = [
-            'originLocationCode'     => 'BOS',
-            'destinationLocationCode' => 'PAR',
-            'departureDate'           => '2021-12-27',
-            'adults'                  => 1
+            'originDestinations' => [
+                [
+                    'id' => 1,
+                    'originLocationCode' => 'BOS',
+                    'destinationLocationCode' => 'PAR',
+                    'departureDateTimeRange' => [
+                        'date' => '2021-12-27'
+                    ]
+                ]
+            ],
+            'travelers' => [
+                [
+                    'id' => 1,
+                    'travelerType' => 'ADULT'
+                ]
+            ],
+            'sources' => [
+                'GDS'
+            ]
         ];
 
-        // To covert key value pairs into query parameters
-        $data = http_build_query($data);
-
-        // Append the query parameters to the URL
-        $url .= '?' . $data;
-
         try {
-            $response = $client->get($url, [
+            $response = $client->post($url, [
                 'headers' => [
                     'Accept' => 'application/json',
                     'Authorization' => 'Bearer ' . $access_token
                 ],
+                'json' => $data
             ]);
 
             return $response->getBody();
         } catch (GuzzleException $exception) {
-            dd($exception);
+            return $exception;
         }
     }
 }
