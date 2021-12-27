d1e4791c7c299ddb0991f97796192c2180b8b68e
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2021-12-27 23:22:43 +0100

    General app layout

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-light bg-light fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Flight App</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Flight App</h5>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Get Price</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Confirm Order</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
